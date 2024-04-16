406f9860261bfc170aa1257b04d4e3d795158735
Chloe <chloe@pridefully.gay>
2024-04-15 22:07:11 -0400

    shark-error: Snippet viewing

--- crates/shark-error/src/source.rs
+++ crates/shark-error/src/source.rs
@@ -27,6 +27,10 @@ impl<'a> SourcePosition<'a> {
         self.column = 1;
     }
 
+    pub fn is_between_row(&self, start: &Self, end: &Self) -> bool {
+        start.row <= self.row && end.row >= self.row
+    }
+
     pub fn stringify(&self) -> String {
         if self.origin.is_none() {
             return format!("unknown:{}:{}", self.row, self.column);
@@ -63,5 +67,87 @@ impl<'a> PartialOrd for SourcePosition<'a> {
         }
 
         Some(std::cmp::Ordering::Less)
+    }
+}
+
+pub struct SharkSnippet<'a> {
+    pub snippet: String,
+    pub start_position: SourcePosition<'a>,
+    pub end_position: SourcePosition<'a>,
+}
+
+impl<'a> SharkSnippet<'a> {
+    pub fn new(
+        source: &str,
+        start_position: SourcePosition<'a>,
+        end_position: SourcePosition<'a>,
+    ) -> Self {
+        let chars = source.chars();
+        let mut snippet: String = String::new();
+
+        let mut current_line = String::new();
+        let mut current_position = SourcePosition::default();
+
+        for current_char in chars {
+            if current_position.is_between_row(&start_position, &end_position) {
+                current_line.push(current_char);
+            }
+
+            current_position.next();
+            if current_char == '\n' {
+                Self::style_new_line(&mut snippet, &mut current_line, &mut current_position);
+            }
+        }
+
+        Self {
+            snippet,
+            start_position,
+            end_position,
+        }
+    }
+
+    pub fn underline(
+        &mut self,
+        start_position: SourcePosition<'a>,
+        underline_length: usize,
+        message: Option<String>,
+    ) {
+        let raw_message = match message {
+            Some(message) => message,
+            None => String::new(),
+        };
+        let line_data = format!(
+            "\n  | {}{} {}",
+            " ".repeat(start_position.column - 1),
+            "^".repeat(underline_length),
+            raw_message
+        );
+
+        let mut new_snippet = String::new();
+        let mut current_row = self.start_position.row;
+        for char in self.snippet.chars() {
+            if current_row == start_position.row && char == '\n' {
+                new_snippet.push_str(line_data.as_str());
+            }
+
+            new_snippet.push(char);
+
+            if char == '\n' {
+                current_row += 1;
+            }
+        }
+        self.snippet = new_snippet
+    }
+
+    fn style_new_line(
+        snippet: &mut String,
+        current_line: &mut String,
+        current_position: &mut SourcePosition,
+    ) {
+        if !current_line.is_empty() {
+            snippet.push_str(format!("{} | {}", current_position.row, current_line).as_str());
+        }
+        current_position.newline();
+        current_line.clear();
     }
 }
