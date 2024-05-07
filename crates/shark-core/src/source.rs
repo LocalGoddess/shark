ee46448e848518d8650bd74424f518f54003c186
Chloe <chloe@pridefully.gay>
2024-05-07 18:23:36 -0400

    shark-core: Refactor SourcePosition

---
+++ crates/shark-core/src/source.rs
@@ -0,0 +1,54 @@
+use std::{fmt::Display, path::Path};
+
+/// Represents a position within a source file. If the path is [Option::None], then this will just
+/// represent a line and column in any source file
+#[derive(Debug, Clone, Copy)]
+pub struct SourcePosition<'position> {
+    pub path: Option<&'position Path>,
+    pub line: usize,
+    pub column: usize,
+}
+
+impl<'position> SourcePosition<'position> {
+    pub fn new(path: Option<&'position Path>, line: usize, column: usize) -> Self {
+        Self { path, line, column }
+    }
+}
+
+impl<'position> Display for SourcePosition<'position> {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let path_str: &str = self
+            .path
+            .as_ref()
+            .and_then(|x| x.to_str())
+            .map_or_else(|| "unknown", |x| x);
+        write!(formatter, "{}:{}:{}", path_str, self.line, self.column)
+    }
+}
+
+impl<'position> PartialOrd for SourcePosition<'position> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        // If both of these position are attached to files we make sure the files are the same
+        // If one or both aren't tied to a file, we don't care
+        if let (Some(self_path), Some(other_path)) = (&self.path, &other.path) {
+            if self_path != other_path {
+                return None;
+            }
+        }
+
+        self.line.partial_cmp(&other.line)
+    }
+}
+
+impl<'position> PartialEq for SourcePosition<'position> {
+    fn eq(&self, other: &Self) -> bool {
+        // Same as above, if both positions have a file attached to them, make sure they are the
+        // smae file
+        if let (Some(self_path), Some(other_path)) = (&self.path, &other.path) {
+            if self_path != other_path {
+                return false;
+            }
+        }
+        self.line == other.line && self.column == other.column
+    }
+}
